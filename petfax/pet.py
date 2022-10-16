49811e1fb848cf2bd3d264aeb69a596f2d8f0fed
CRgarcia15 <cg58708@gmial.com>
2022-10-16 13:41:38 -0400

    update. Completed activity

--- petfax/pet.py
+++ petfax/pet.py
@@ -1,11 +1,13 @@
 from flask import (Blueprint, render_template)
 import json
 
+#Middleware
 bp = Blueprint('pet', __name__, url_prefix="/pets")
 
 pets = json.load(open('pets.json'))
 print(pets)
 
+#Routes
 @bp.route('/')
 def index():
     return render_template('index.html', pets = pets)
